58a81ec0a738aad9e161ab3d97671eae055b425d
AxelDemorest <axel.demorest@gmail.com>
2021-03-20 18:22:03 +0100

    Création du header

---
+++ components/header.php
@@ -0,0 +1,18 @@
+<div class="container_top_bar">
+    <div class="content_top_bar">
+        <div class="content_left_top_bar">
+            <div class="logo_left_top_bar">
+                <h2>Swancy</h2>
+            </div>
+        </div>
+        <div class="content_right_top_bar">
+            <ul class="list_navigation">
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Explorer</a></li>
+                <li><a href="">Notifications</a></li>
+                <li><a href="">Messages</a></li>
+                <li><a href="">Profil</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
