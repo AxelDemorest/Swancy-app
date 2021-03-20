7fee02ca29bdb2da485286589ee0c88b5100caec
AxelDemorest <axel.demorest@gmail.com>
2021-03-20 18:22:49 +0100

    Développement des premières pages

---
+++ pages/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../components/css/header.css">
+    <link rel="stylesheet" href="../public/css/init.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <title>Swancy App</title>
+</head>
+
+<body>
+
+    <?php require "../components/header.php"; ?>
+
+    <div>
+
+    </div>
+
+</body>
+
+</html>
---
+++ pages/inscription.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../components/css/header.css">
+    <link rel="stylesheet" href="../public/css/init.css">
+    <link rel="stylesheet" href="../public/css/inscription.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <title>Swancy App</title>
+</head>
+
+<body>
+
+    <div class="container_page">
+        <div class="bloc_presentation">
+            <h1>Viens découvrir les utilisateurs de Swancy</h1>
+        </div>
+        <div class="form_inscription">
+            <form action="" method="POST">
+                <h2>Logo</h2>
+                <div class="form_label">
+                    <input type="text" name="" placeholder="Nom d'utilisateur">
+                </div>
+                <div class="form_label">
+                    <input type="text" name="" placeholder="Adresse e-mail">
+                </div>
+                <div class="form_label">
+                    <input type="text" name="" placeholder="Mot de passe">
+                </div>
+                <div class="form_label">
+                    <input type="text" name="" placeholder="Confirmation du mot de passe">
+                </div>
+                <div class="form_label">
+                    <p>Tu possèdes déjà un compte ? <a href="">Connecte toi ici</a></p>
+                    <input type="submit" name="" value="S'inscrire">
+                </div>
+            </form>
+        </div>
+    </div>
+
+</body>
+
+</html>
